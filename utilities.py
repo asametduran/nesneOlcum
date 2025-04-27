cc7139ee93c113e370becfd4ec57fc0225f8ab0e
asametduran <asametdurann@gmail.com>
2025-04-27 18:15:01 +0300

    acma ve kapama islemleri yapildi

--- utilities.py
+++ utilities.py
@@ -5,4 +5,7 @@ def getContours(img,cannyThreshHold=[100,100],showCanny=False):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Convert to grayscale
     imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1) # Apply Gaussian blur
     imgCanny = cv2.Canny(imgBlur, cannyThreshHold[0],cannyThreshHold[1]) # Apply Canny edge detection
-    if showCanny:cv2.imshow('Canny',imgCanny) # Show Canny image
+    kernel = np.ones((5, 5))
+    imgDial = cv2.dilate(imgCanny, kernel, iterations=3) # Dilate the image
+    imgThre = cv2.erode(imgDial, kernel, iterations=2) # Erode the image
+    if showCanny:cv2.imshow('Canny',imgThre) # Show Canny image
