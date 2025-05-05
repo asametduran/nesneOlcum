20056c6eea71486a2ef91c6296d1e72c76706db3
asametduran <asametdurann@gmail.com>
2025-05-05 14:49:52 +0300

    Kontürleme işlemleri yapıldı

--- utilities.py
+++ utilities.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def getContours(img,cannyThreshHold=[100,100],showCanny=False):
+def getContours(img,cannyThreshHold=[100,100],showCanny=False, minArea=1000):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Convert to grayscale
     imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1) # Apply Gaussian blur
     imgCanny = cv2.Canny(imgBlur, cannyThreshHold[0],cannyThreshHold[1]) # Apply Canny edge detection
@@ -9,3 +9,12 @@ def getContours(img,cannyThreshHold=[100,100],showCanny=False):
     imgDial = cv2.dilate(imgCanny, kernel, iterations=3) # Dilate the image
     imgThre = cv2.erode(imgDial, kernel, iterations=2) # Erode the image
     if showCanny:cv2.imshow('Canny',imgThre) # Show Canny image
+
+    contours, hierarchy = cv2.findContours(imgThre, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # Find contours
+
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > minArea:
+            peri = cv2.arcLength(i, True) # Get perimeter, closed
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True) # Approximate the contour to a polygon
+            boundingBox = cv2.boundingRect(approx) # Get bounding box
