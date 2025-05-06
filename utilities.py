4085320b0c7a7623b64a615ff3684103765f48c9
asametduran <asametdurann@gmail.com>
2025-05-06 23:27:47 +0300

    Filtreleme kısmı eklendi

--- utilities.py
+++ utilities.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def getContours(img,cannyThreshHold=[100,100],showCanny=False, minArea=1000):
+def getContours(img,cannyThreshHold=[100,100],showCanny=False, minArea=1000,filter=0):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Convert to grayscale
     imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1) # Apply Gaussian blur
     imgCanny = cv2.Canny(imgBlur, cannyThreshHold[0],cannyThreshHold[1]) # Apply Canny edge detection
@@ -11,10 +11,17 @@ def getContours(img,cannyThreshHold=[100,100],showCanny=False, minArea=1000):
     if showCanny:cv2.imshow('Canny',imgThre) # Show Canny image
 
     contours, hierarchy = cv2.findContours(imgThre, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # Find contours
-
+    finalContours = [] # List to store final contours
     for i in contours:
         area = cv2.contourArea(i)
         if area > minArea:
             peri = cv2.arcLength(i, True) # Get perimeter, closed
             approx = cv2.approxPolyDP(i, 0.02 * peri, True) # Approximate the contour to a polygon
             boundingBox = cv2.boundingRect(approx) # Get bounding box
+            if filter > 0 :
+                if len(approx) == filter:
+                    finalContours.append([len(approx),area, approx, boundingBox, i])
+                else:
+                    finalContours.append([len(approx),area, approx, boundingBox, i])
+
+
