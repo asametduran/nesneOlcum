464950d7ae632583e674e7712b5a56a191de1b2f
asametduran <asametdurann@gmail.com>
2025-06-05 15:29:44 +0300

    reshaped

--- objectMeasurement.py
+++ objectMeasurement.py
@@ -3,7 +3,7 @@ import cv2
 import utilities as ut
 
 webcam = False # Set to True if using a webcam, False if using a picture or video file
-path = '1.jpg'
+path = '2.jpg'
 cap = cv2.VideoCapture(0) 
 cap.set(10, 160) # Brightness
 cap.set(3, 1920) # Width
@@ -14,7 +14,13 @@ while True:
     if webcam:success, img = cap.read()
     else: img = cv2.imread(path)
 
-    ut.getContours(img,showCanny=True) # Get contours from the image
+    finalImg, finalContours = ut.getContours(img,showCanny=True,
+                                            minArea=50000,filter=4) # Get contours from the image
+    
+    if len(finalContours) != 0:
+        biggestContour = finalContours[0][2]
+        print(biggestContour) #u çıkıtyı alıyorsun 891,197   146,201     117,1669     934 1667       4 1 2
+        ut.warpImg(finalImg, biggestContour, 100, 100) # Warp the image using the biggest contour
 
     img = cv2.resize(img, (0,0),None,0.5,0.5)
     cv2.imshow('Original',img)
--- utilities.py
+++ utilities.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def getContours(img,cannyThreshHold=[100,100],showCanny=False, minArea=1000,filter=0):
+def getContours(img,cannyThreshHold=[100,100],showCanny=False, minArea=1000,filter=0, draw=False):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Convert to grayscale
     imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1) # Apply Gaussian blur
     imgCanny = cv2.Canny(imgBlur, cannyThreshHold[0],cannyThreshHold[1]) # Apply Canny edge detection
@@ -24,4 +24,28 @@ def getContours(img,cannyThreshHold=[100,100],showCanny=False, minArea=1000,filt
                 else:
                     finalContours.append([len(approx),area, approx, boundingBox, i])
 
+    finalContours = sorted(finalContours, key=lambda x: x[1], reverse=True) # Sort contours by area, largest first
 
+    if draw:
+        for con in finalContours:
+            cv2.drawContours(img, con[4], -1, (0, 0, 255), 3)
+
+    return img, finalContours
+
+def reorder(myPoints):
+    print(myPoints.shape)
+    myPointsNew = np.zeros_like(myPoints) # Create a new array with the same shape as myPoints
+    myPoints = myPoints.reshape((4,2))
+    add = myPoints.sum(1) # Sum of x and y coordinates
+    myPointsNew[0] = myPoints[np.argmin(add)] # Top left point
+    myPointsNew[3] = myPoints[np.argmax(add)] # Bottom right point
+    diff = np.diff(myPoints, axis=1) # Difference between x and y coordinates
+    myPointsNew[1] = myPoints[np.argmin(diff)] # Top right point
+    myPointsNew[2] = myPoints[np.argmax(diff)] # Bottom left point
+    return myPointsNew
+
+def warpImg(img, points, w, h):
+    print(points)
+    reorder(points)
+
+    
